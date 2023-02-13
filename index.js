4a1f2661da29fc7cb0be9fd381f357c6832acf28
gedeon <otakuagbolossou@gmail.com>
2023-02-13 23:39:44 +0000

    bases du js

---
+++ index.js
@@ -0,0 +1,23 @@
+/* var nom ='bruh'
+
+let nom ='bruh'
+
+{
+    //
+
+    var chien = "bruh"
+    let ok = "bruh"
+
+}
+
+function saluer(){
+
+    console.log("saluer")
+} */
+
+function saluerPersonne(nom){
+
+    console.log("salut " + nom)
+}
+
+saluerPersonne("gedeon")
